d22bf8cf8244f243a1e428ff8d9a9d800f9487e5
UiYukinaga <i.yukinaga@me.com>
2020-11-05 15:11:10 +0900

    1st Commit

---
+++ rotate_cw.py
@@ -0,0 +1,42 @@
+# Telloが定常円旋回をするコード
+# Argument: 何回転するか
+
+from time import sleep
+import tellopy
+import sys
+
+def handler(event, sender, data, **args):
+    drone = sender
+    if event is drone.EVENT_FLIGHT_DATA:
+        print(data)
+
+
+def rotate_cw(n_rote):
+    drone = tellopy.Tello()
+    secs_per_rotate = 15
+    flight_time = n_rote * secs_per_rotate
+    
+    try:
+        drone.subscribe(drone.EVENT_FLIGHT_DATA, handler)
+        drone.connect()
+        drone.wait_for_connection(60.0)
+        drone.takeoff()
+        sleep(5)
+        
+        # 定常円旋回は、前進とその場旋回のベクトル和で実現
+        drone.forward(20)   # 前進飛行速度=20cm/s
+        drone.clockwise(36) # 旋回方向と角速度を設定(時計回り, 36deg/s)
+        sleep(flight_time)
+        drone.land()
+        sleep(5)
+
+    except Exception as ex:
+        drone.land() #エラーが起きたらその場で緊急着陸
+        print(ex)
+    finally:
+        drone.quit()
+
+if __name__ == '__main__':
+    args = sys.argv
+    deg = int(args[1])
+    rotate_cw(deg)
